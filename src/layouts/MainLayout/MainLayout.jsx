de3d1a17729e3514a370f1470ae7efbacf089b37
luokaiwu <luokaiwu@wodejingjiren.com>
2016-09-21 20:52:10 +0800

    init

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import { Router, Route, IndexRoute, Link } from 'react-router';
+import styles from './MainLayout.less';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.head}>
+        <h1>Spring Annotation</h1>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.side}>
+          <h2>筛选:</h2>
+          <Link to="/">All</Link><br />
+          <Link to="/">Actived</Link><br />
+          <Link to="/completed">Completed</Link><br />
+          <Link to="/404">404</Link><br />
+        </div>
+        <div className={styles.main}>
+          {children}
+        </div>
+      </div>
+      <div className={styles.foot}>
+        Built with react, react-router, ant-tool, css-modules, antd...
+      </div>
+    </div>
+  );
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default MainLayout;
